ccb04308dcd0f1f539bfb4b3a493f140214f4eeb
Sahil Kalkal <sahil@flyde.in>
2025-11-25 14:58:19 +0530

    auto generated

---
+++ components/componentRegistry.jsx
@@ -0,0 +1,20 @@
+
+import Header from "./header";
+import GridVideo from "./GridVideo";
+import WhyChoose from "./whyChoose";
+import FeaturesSection from "./featuresSection";
+import SectionHeader from "./sectionHeader";
+import GridSection from "./GridWrapper";
+import ImageCard from "./card";
+import HeroSection from "./heroSection";
+
+export const componentRegistry = {
+  "header-sec": Header,
+  "grid-video": GridVideo,
+  "why-choose": WhyChoose,
+  "feature-section": FeaturesSection,
+  "section-header": SectionHeader,
+  "grid-section": GridSection,
+  "image-card": ImageCard,
+  "hero-section": HeroSection
+};
